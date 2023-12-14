6d7548141a13e163ef295972200c55d65c6165e3
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-14 21:09:29 +0600

    all done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,17 @@
 const Hero = () => {
   return (
     <div>
-      <h1>Hero</h1>
+      <h1>
+        Hi 👋 <br />
+        I’m Mahdi Mortuza
+      </h1>
+      <p>
+        A full-stack web developer. I solve web problems by crafting scalable
+        products using MERN stack technology.
+      </p>
+      <button> Talk</button>
+
+      <button> My Resume</button>
     </div>
   );
 };

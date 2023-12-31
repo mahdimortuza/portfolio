e78ec4d2d05b982e38482ad037ece7fa8f518410
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-31 10:54:03 +0600

    components added

---
+++ src/components/ProjectIdeaCTA.jsx
@@ -0,0 +1,9 @@
+const ProjectIdeaCTA = () => {
+  return (
+    <div>
+      <h1>Have a project idea?</h1>
+    </div>
+  );
+};
+
+export default ProjectIdeaCTA;
---
+++ src/components/Technologies.jsx
@@ -0,0 +1,9 @@
+const Technologies = () => {
+  return (
+    <div>
+      <h1>technologies i use</h1>
+    </div>
+  );
+};
+
+export default Technologies;

55ab0d8b1fd0f1db7f5d8748391c4f9b0421b81e
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-31 21:11:25 +0600

    swiper added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,10 @@
+import "swiper/css";
+import "swiper/css/effect-cards";
+import { EffectCards } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import objBlue from "../assets/bg-objects/obj-blue.svg";
 import sliderObj from "../assets/bg-objects/sliderObj.svg";
+
 const Projects = () => {
   return (
     <div className="max-w-[1350px] mx-auto relative">
@@ -8,6 +13,16 @@ const Projects = () => {
         Some of my recent <br /> projects
       </h2>
       <img className="absolute top-0 left-0" src={sliderObj} />
+
+      {/* swiper slider section starts here  */}
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper mt-8"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+      </Swiper>
     </div>
   );
 };

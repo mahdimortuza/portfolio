bd4c500b33d92faa92e7665e29854703008f9a08
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-31 20:18:17 +0600

    slider

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,13 @@
+import objBlue from "../assets/bg-objects/obj-blue.svg";
+import sliderObj from "../assets/bg-objects/sliderObj.svg";
 const Projects = () => {
   return (
-    <div>
-      <h2>recent projects</h2>
+    <div className="max-w-[1350px] mx-auto relative">
+      <img className="absolute top-0 right-0 -mt-96" src={objBlue} />
+      <h2 className="font-playfair text-[64px] text-center leading-[66px] mt-[100px]">
+        Some of my recent <br /> projects
+      </h2>
+      <img className="absolute top-0 left-0" src={sliderObj} />
     </div>
   );
 };
